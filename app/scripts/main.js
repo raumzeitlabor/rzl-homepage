b8c5dad4ab46aba7188b76205e2672fdc6f82c11
Simon Elsbrock <simon@iodev.org>
2014-08-09 17:04:30 +0200

    /kontakt/anfahrt: Add dynamic information
    If the space is open, the greeting text is changed accordingly.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,10 +2,13 @@
     'use strict';
     $.getJSON('http://s.rzl.so/api/simple.json', function(d) {
         if (d === 1) {
+            $('#rzl-status').show();
             $('.raumstatus').removeClass('label-warning label-error').addClass('label-success').empty().append('offen');
         } else if (d === 0) {
+            $('#rzl-status').hide();
             $('.raumstatus').removeClass('label-warning label-error').addClass('label-error').empty().append('geschlossen');
         } else {
+            $('#rzl-status').hide();
             $('.raumstatus').removeClass('label-success label-error').addClass('label-warning').empty().append('unbekannt');
         }
     })
