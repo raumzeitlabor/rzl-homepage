853f59094ff403b533d2cd9cbe533a1634b4d414
Simon Elsbrock <simon@iodev.org>
2014-08-10 12:04:30 +0200

    Fix wrong label for closed space

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,16 +3,16 @@
     $.getJSON('http://s.rzl.so/api/simple.json', function(d) {
         if (d === 1) {
             $('#rzl-status').show();
-            $('.raumstatus').removeClass('label-warning label-error').addClass('label-success').empty().append('offen');
+            $('.raumstatus').removeClass('label-warning label-danger').addClass('label-success').empty().append('offen');
         } else if (d === 0) {
             $('#rzl-status').hide();
-            $('.raumstatus').removeClass('label-warning label-error').addClass('label-error').empty().append('geschlossen');
+            $('.raumstatus').removeClass('label-warning label-danger').addClass('label-danger').empty().append('geschlossen');
         } else {
             $('#rzl-status').hide();
-            $('.raumstatus').removeClass('label-success label-error').addClass('label-warning').empty().append('unbekannt');
+            $('.raumstatus').removeClass('label-success label-danger').addClass('label-warning').empty().append('unbekannt');
         }
     })
     .fail(function() {
-        $('.raumstatus').removeClass('label-success label-error').addClass('label-warning').empty().append('unbekannt');
+        $('.raumstatus').removeClass('label-success label-danger').addClass('label-warning').empty().append('unbekannt');
     });
 })();
