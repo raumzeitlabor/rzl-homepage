d30d26554759a24d8b203c26acc751eddca4370c
Simon Elsbrock <simon@iodev.org>
2014-08-22 00:26:27 +0200

    Improve calendar style; same-day events same-side
    Adds a separator between headline and description. Adds a time string
    underneath event name. If a event happens on the same day like one of
    its predecessors, it will appear on the same side.

--- app/scripts/events.js
+++ app/scripts/events.js
@@ -13,17 +13,36 @@
 
         $('#rzl-events').prepend('<h1>' + moment(d.month, 'MM-YYYY').format('MMMM YYYY') + '</h1>');
 
+        var prevDate = null;
+        var currentSide = 'left';
         d.data.forEach(function(ev, i) {
+            var currDate = moment(ev.timeBegin);
+            if (prevDate === null) {
+                prevDate = currDate;
+            }
+
             var c = $($('#rzl-event-tmpl').html());
             c.find('.rzl-event-date time').append(moment(ev.timeBegin).format('[<span>]DD[</span><span>]dd[</span>]'));
+
+            if (moment().subtract(1, 'days') > moment(ev.timeBegin)) {
+                c.find('.rzl-event-fromnow').append(moment(ev.timeBegin).fromNow());
+            } else {
+                c.find('.rzl-event-fromnow').append(moment(ev.timeBegin).format('dddd, Do MMMM [ab] hh:mm [Uhr]'));
+            }
+
             c.find('.rzl-event-name').append(ev.name);
             c.find('.rzl-event-descr').append(marked(ev.description.replace(/#/g, '###')));
-            if (i % 2 === 0) {
-                c.find('.rzl-event-left').show();
-            } else {
-                c.find('.rzl-event-right').show();
+
+            if (currDate.date() !== prevDate.date()) {
+                currentSide = (currentSide === 'left') ? 'right' : 'left';
+            } else if (i > 0) {
+                c.find('.rzl-event-date').hide();
             }
+
+            c.find('.rzl-event-' + currentSide).show();
             $('#rzl-events').append(c);
+
+            prevDate = currDate;
         });
     }).fail(function() {
         $('#rzl-events').empty().append('<h2>Fehler</h2><p>' + errmsg + '</p>');
