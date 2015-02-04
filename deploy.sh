b11339651ab0b7fc7877591ccf9d927da01acec9
Simon Elsbrock <simon@iodev.org>
2015-02-04 22:02:33 +0100

    Let Travis do the deployment of the homepage
    This commit adds an after_install Travis hook. The hook first ensures it
    is running within a Travis environment, decrypts the ssh deployment key
    and initializes an ssh-agent, and then copies all files from dist to
    premium.
    
    The deployment script is based on the following gist:
    
      https://gist.github.com/pghalliday/240fe740d523dad21d3f
    
    The ssh deployment key is stored encrypted in the repository (AES-256).
    More information on this can be found here:
    
      http://docs.travis-ci.com/user/encrypting-files/

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+# Sync the contents of this directory where the site should have been built
+SOURCE_DIR=dist
+# Where to copy files
+TARGET_DIR=/var/www/www.raumzeitlabor.de/htdocs
+
+if [ ! -d "$SOURCE_DIR" ]; then
+  echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
+  exit 1
+fi
+
+if [ -n "$TRAVIS_BUILD_ID" ]; then
+  # Set the following environment variables in the travis configuration (.travis.yml)
+  #
+  #   DEPLOY_BRANCH    - The only branch that Travis should deploy from
+  #   ENCRYPTION_LABEL - The label assigned when encrypting the SSH key using travis encrypt-file
+  #
+  echo DEPLOY_BRANCH: $DEPLOY_BRANCH
+  echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
+  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+    echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
+    exit 0
+  else
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+      echo "Travis should not deploy from pull requests"
+      exit 0
+    else
+      ENCRYPTED_KEY_VAR=encrypted_${ENCRYPTION_LABEL}_key
+      ENCRYPTED_IV_VAR=encrypted_${ENCRYPTION_LABEL}_iv
+      ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+      ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+      # The `deploy_key.enc` file should have been added to the repo and should
+      # have been created from the deploy private key using `travis encrypt-file`
+      openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+
+      chmod 600 deploy_key
+      eval `ssh-agent -s`
+      ssh-add deploy_key
+    fi
+  fi
+fi
+
+rsync -zvrt --delete --checksum -e ssh $SOURCE_DIR/ rzl-homepage@premium.raumzeitlabor.de:$TARGET_DIR/
