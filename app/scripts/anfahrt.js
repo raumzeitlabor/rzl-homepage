b0970b82bc0e02afa08825d46c45ae6ccc2447d0
Tobias Rehbein <tobias.rehbein@web.de>
2015-02-10 19:26:27 +0100

    Add map overlays
    Add map overlays showing the paths described in the text.
    
    While here make the way from the two different gates to the RaumZeitLabor two
    different steps.
    
    Closes raumzeitlabor/rzl-homepage#18

--- app/scripts/anfahrt.js
+++ app/scripts/anfahrt.js
@@ -18,31 +18,65 @@
     }).addTo(map);
 
     var marker = L.marker([49.50526, 8.502]).addTo(map);
+    var polyline = L.polyline([ ], {color: 'red'}).addTo(map);
     $(window).on('hashchange', function() {
         var stepHash = location.hash.replace('#', '');
         switch (stepHash) {
             default:
             case 'step1':
                 marker.setLatLng([49.505370187170236, 8.50215196609497]).update();
+                polyline.setLatLngs([ ]);
                 map.setView(marker.getLatLng(), 15);
                 break;
             case 'step2':
                 marker.setLatLng([49.505370187170236, 8.50215196609497]).update();
-                map.setView(marker.getLatLng(), 17);
+                polyline.setLatLngs([
+                    [49.505370187170236, 8.50215196609497],
+                    [49.505121, 8.501657],
+                    [49.50734182456017, 8.499072790145874]
+                ]);
+                map.fitBounds(polyline.getBounds());
                 break;
             case 'step3':
                 marker.setLatLng([49.50734182456017, 8.499072790145874]).update();
+                polyline.setLatLngs([ ]);
                 map.setView(marker.getLatLng(), 17);
                 $('#step5-prev').attr('href', '#step3');
                 break;
             case 'step4':
                 marker.setLatLng([49.50810816393884, 8.500746488571167]).update();
-                map.setView(marker.getLatLng(), 17);
+                polyline.setLatLngs([
+                    [49.50734182456017, 8.499072790145874],
+                    [49.508662, 8.497355],
+                    [49.509798, 8.499474],
+                    [49.508355, 8.501255],
+                    [49.50810816393884, 8.500746488571167]
+                ]);
+                map.fitBounds(polyline.getBounds());
                 $('#step5-prev').attr('href', '#step4');
                 break;
             case 'step5':
                 marker.setLatLng([49.50804546389515, 8.499341011047363]).update();
-                map.setView(marker.getLatLng(), 17);
+                polyline.setLatLngs([
+                    [49.50810816393884, 8.500746488571167],
+                    [49.507293, 8.499185],
+                    [49.508028, 8.498257],
+                    [49.508349, 8.498873],
+                    [49.508018, 8.499297],
+                    [49.50804546389515, 8.499341011047363]
+                ]);
+                map.fitBounds(polyline.getBounds());
+                break;
+            case 'step6':
+                marker.setLatLng([49.50804546389515, 8.499341011047363]).update();
+                polyline.setLatLngs([
+                    [49.507293, 8.499185],
+                    [49.508028, 8.498257],
+                    [49.508349, 8.498873],
+                    [49.508018, 8.499297],
+                    [49.50804546389515, 8.499341011047363]
+                ]);
+                map.fitBounds(polyline.getBounds());
                 break;
         }
 
