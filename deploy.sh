28c364228b4a72b44af2e3596fcdb969d0b095dc
Simon Elsbrock <simon@iodev.org>
2015-03-16 00:31:43 +0100

    Prepare Travis to deploy PRs

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ set -e
 
 # Sync the contents of this directory where the site should have been built
 SOURCE_DIR=dist
-# Where to copy files
+# Where to copy master
 TARGET_DIR=/data/prod
+# Where to copy pull requests
+PREVIEW_DIR=/data/preview
 
 if [ ! -d "$SOURCE_DIR" ]; then
   echo "SOURCE_DIR ($SOURCE_DIR) does not exist, build the source directory before deploying"
@@ -19,28 +21,33 @@ if [ -n "$TRAVIS_BUILD_ID" ]; then
   #
   echo DEPLOY_BRANCH: $DEPLOY_BRANCH
   echo ENCRYPTION_LABEL: $ENCRYPTION_LABEL
-  if [ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
+  if [[ "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
     echo "Travis should only deploy from the DEPLOY_BRANCH ($DEPLOY_BRANCH) branch"
     exit 0
   else
     if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-      echo "Travis should not deploy from pull requests"
-      exit 0
-    else
-      ENCRYPTED_KEY_VAR=encrypted_${ENCRYPTION_LABEL}_key
-      ENCRYPTED_IV_VAR=encrypted_${ENCRYPTION_LABEL}_iv
-      ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-      ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-
-      # The `deploy_key.enc` file should have been added to the repo and should
-      # have been created from the deploy private key using `travis encrypt-file`
-      openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-
-      chmod 600 deploy_key
-      eval `ssh-agent -s`
-      ssh-add deploy_key
+      echo "Deploying pull request to http://pr-$TRAVIS_PULL_REQUEST.rzl-homepage.raumzeitlabor.org/"
+      ORIG_TARGET_DIR=$TARGET_DIR
+      TARGET_DIR=$PREVIEW_DIR/pr-$TRAVIS_PULL_REQUEST
     fi
+
+    ENCRYPTED_KEY_VAR=encrypted_${ENCRYPTION_LABEL}_key
+    ENCRYPTED_IV_VAR=encrypted_${ENCRYPTION_LABEL}_iv
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+    # The `deploy_key.enc` file should have been added to the repo and should
+    # have been created from the deploy private key using `travis encrypt-file`
+    openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+
+    chmod 600 deploy_key
+    eval `ssh-agent -s`
+    ssh-add deploy_key
   fi
 fi
 
-rsync -zvrt --omit-dir-times --delete --checksum -e ssh $SOURCE_DIR/ deploy@rzl-homepage.docker.raumzeitlabor.de:$TARGET_DIR
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+    rsync -zvrt --omit-dir-times --delete --checksum --link-dest=$ORIG_TARGET_DIR -e ssh $SOURCE_DIR/ deploy@rzl-homepage.docker.raumzeitlabor.de:$TARGET_DIR ;
+else
+    rsync -zvrt --omit-dir-times --delete --checksum -e ssh $SOURCE_DIR/ deploy@rzl-homepage.docker.raumzeitlabor.de:$TARGET_DIR
+fi
