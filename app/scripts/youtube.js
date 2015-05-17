16caf4226bc635d4bce8dfc406c1a70e29ed29f1
Simon Elsbrock <simon@iodev.org>
2015-05-17 13:18:10 +0200

    /videos: Switch to new Youtube v3 API
    The old API was deprecated on 20th of April, see [1]. We access the new
    API using a browser token. This token is whitelisted for the following
    referers:
    
        *.raumzeitlabor.de/*
        *.raumzeitlabor.org/*
        localhost/*
    
    The token is owned by me and can easily be replaced by someone else's
    token.
    
    At the same time, we updated the CSP for the new central Google API
    endpoint on github.com/raumzeitlabor/rzl-homepage-docker [2].
    
    closes #137
    
    [1] https://support.google.com/youtube/answer/6098135?p=yt_devicesupport&hl=de&rd=1
    [2] https://github.com/raumzeitlabor/rzl-homepage-docker/commit/0d5576aa17f73b758e84b3614aa5aac035f2e6cf

--- app/scripts/youtube.js
+++ app/scripts/youtube.js
@@ -11,28 +11,28 @@ $(document).ready(function() {
         }
 
         // newest video is teasered
-        var lsTime = moment(v[0].uploaded).format('LL');
-        $('<h3 style="margin-top:0">' + v[0].title + '</h3><p><i class="glyphicon glyphicon-time"></i><span style="font-style: italic; color: #8e8e8e"> hochgeladen: ' + lsTime + '</span><p>' + v[0].description + '</p><p><em>weiter zu <a href="' + v[0].player.default + '">Youtube</a></em></p>')
+        var lsTime = moment(v[0].snippet.publishedAt).format('LL');
+        $('<h3 style="margin-top:0">' + v[0].snippet.title + '</h3><p><i class="glyphicon glyphicon-time"></i><span style="font-style: italic; color: #8e8e8e"> hochgeladen: ' + lsTime + '</span><p>' + v[0].snippet.description + '</p><p><em>weiter zu <a href="https://youtu.be/' + v[0].snippet.resourceId.videoId + '">Youtube</a></em></p>')
             .appendTo('#youtube-main-descr');
-        $('<iframe width="560" height="315" src="//www.youtube.com/embed/' + v[0].id + '?hd=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>')
+        $('<iframe width="560" height="315" src="//www.youtube.com/embed/' + v[0].snippet.resourceId.videoId + '?hd=1&autohide=1&showinfo=0" frameborder="0" allowfullscreen></iframe>')
             .appendTo('#youtube-main');
 
         if (v.length < 2) {
             return;
         }
 
-        for (var i = 2; i < v.length; i++) {
-            lsTime = moment(v[i].uploaded).format('LL');
+        for (var i = 1; i < v.length; i++) {
+            lsTime = moment(v[i].snippet.publishedAt).format('LL');
             var row = $('<div class="row" style="margin:1.5em; margin-left: 0">');
-            $('<div class="col-lg-4 col-md-4 col-sm-12 col-sx-12" id="youtube-item-thumb"><img src="' + v[i].thumbnail.hqDefault + '" /></div>')
+            $('<div class="col-lg-4 col-md-4 col-sm-12 col-sx-12" id="youtube-item-thumb"><img src="' + v[i].snippet.thumbnails.high.url + '" /></div>')
                 .appendTo(row);
-            $('<div class="col-lg-8 col-md-8 col-sm-12 col-sx-12" id="youtube-item-descr"><h3 style="margin-top:0">' + v[i].title + '</h3><p><i class="glyphicon glyphicon-time"></i><span style="font-style: italic; color: #8e8e8e"> hochgeladen: ' + lsTime + '</span><p>' + v[i].description + '</p><p><em>weiter zu <a href="' + v[i].player.default + '">Youtube</a></em></p></div>')
+            $('<div class="col-lg-8 col-md-8 col-sm-12 col-sx-12" id="youtube-item-descr"><h3 style="margin-top:0">' + v[i].snippet.title + '</h3><p><i class="glyphicon glyphicon-time"></i><span style="font-style: italic; color: #8e8e8e"> hochgeladen: ' + lsTime + '</span><p>' + v[i].snippet.description + '</p><p><em>weiter zu <a href="https://youtu.be/' + v[i].snippet.resourceId.videoId + '">Youtube</a></em></p></div>')
                 .appendTo(row);
             row.appendTo('#youtube-archive');
         }
     }
 
-    $.getJSON('//gdata.youtube.com/feeds/api/users/raumzeitlabor/uploads?v=2&alt=jsonc&max-results=11&callback=?')
+    $.getJSON('https://content.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUG0GYTzLZNbITXIO63mjWlQ&key=AIzaSyBlDy9ypeC0AshIzQ2FhFTEU1502OUomXg&callback=?')
         .always(function() {
             $('#loading').hide();
         })
@@ -47,6 +47,6 @@ $(document).ready(function() {
                 return;
             }
             $('#youtube-hidden').show();
-            render(d.data.items);
+            render(d.items);
         });
 });
