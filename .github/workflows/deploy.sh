860fc17446022808a4cdccaebdcc0aed23e2251a
Ranlvor <ranlvor@starletp9.de>
2021-11-20 13:39:43 +0100

    first (dummy) test of the new pull-request-code

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Preparing ~/.ssh..."
 mkdir ~/.ssh
@@ -12,7 +12,19 @@ ssh-agent > ~/.ssh/env
 echo "$DEPLOY_OPENSSH_PRIVATE_KEY" | ssh-add -
 unset DEPLOY_OPENSSH_PRIVATE_KEY
 ssh-add -l
+if [ $(ssh-add -l | wc -l) -lt 1 ]; then
+  echo "No ssh key loaded. Stopping deploy."
+  exit
+fi
+
+pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 
 echo "Copying data..."
-rsync -zvrt --omit-dir-times --delete --checksum -e "ssh -4 -p 36270" --progress dist/ deploy@premium-ng.raumzeitlabor.de:/var/lib/www/www.raumzeitlabor.de
+if [ -z "$pull_number" ]; then
+  echo rsync -zvrt --omit-dir-times --delete --checksum -e "ssh -4 -p 36270" --progress dist/ deploy@premium-ng.raumzeitlabor.de:/var/lib/www/www.raumzeitlabor.de
+else
+  echo ssh ssh -4 -p 36270 deploy@premium-ng.raumzeitlabor.de mkdir /var/lib/www/www.raumzeitlabor.de-preview/pr-$pull_number
+  echo rsync -zvrt --omit-dir-times --delete --checksum -e "ssh -4 -p 36270" --progress dist/ deploy@premium-ng.raumzeitlabor.de:/var/lib/www/www.raumzeitlabor.de
+fi
+
 
